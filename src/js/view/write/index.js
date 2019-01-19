e0362bad3393b4060bd1fd490a983054df38798a
tykdn <1564546470@qq.com>
2019-01-19 21:21:09 +0800

    write page

--- src/js/view/write/index.js
+++ src/js/view/write/index.js
@@ -4,48 +4,130 @@ import {
   Text,
   View,
   StyleSheet,
+  TextInput,
   PixelRatio,
-  Switch,
-  ScrollView
+  ScrollView,
+  TouchableOpacity
 } from "react-native";
+import TextButton from "../../component/textButton";
 import theme from "../../config/theme";
 import px2dp from "../../utils/px2dp";
-import Icon from "react-native-vector-icons/Ionicons";
-
+import Avatar from "../../component/avatar";
+class UselessTextInput extends Component {
+  render() {
+    return <TextInput {...this.props} editable={true} maxLength={40} />;
+  }
+}
 export default class Write extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dataArray: [
-        { name: "通知", value: false },
-        { name: "在线状态", value: false },
-        { name: "账号", value: false },
-        { name: "手机", value: false },
-        { name: "用户", value: false },
-        { name: "快拍", value: false },
-        { name: "私密对话", value: false },
-        { name: "流量和存储", value: false },
-        { name: "表情选项", value: false },
-        { name: "切换账户", value: false },
-        { name: "创建新账户", value: false },
-        { name: "账号设置", value: false },
-        { name: "报告问题", value: false },
-        { name: "帮助", value: false },
-        { name: "法律与政策", value: false }
+      text: "",
+      list: [
+        {
+          name: "xiaoxiaomin"
+        },
+        {
+          name: "xiaoxiaomin"
+        },
+        {
+          name: "xiaoxiaomin"
+        },
+        {
+          name: "xiaoxiaomin"
+        },
+        {
+          name: "xiaoxiaomin"
+        },
+        {
+          name: "xiaoxiaomin"
+        },
+        {
+          name: "xiaoxiaomin"
+        }
       ]
     };
+    this.rendlist = this.rendlist.bind(this);
   }
-
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: "新消息",
+      headerLeft: (
+        <View style={{ paddingLeft: px2dp(16) }}>
+          <TextButton
+            onPress={() => navigation.goBack()}
+            text={"取消"}
+            fontSize={px2dp(15)}
+            color={"rgb(92,133,194)"}
+          />
+        </View>
+      ),
+      headerRight: (
+        <View style={{ paddingRight: px2dp(16) }}>
+          <TextButton
+            onPress={() => navigation.goBack()}
+            text={"私密"}
+            fontSize={px2dp(15)}
+            color={"rgb(92,133,194)"}
+          />
+        </View>
+      ),
+      headerTintColor: "#000",
+      headerTitleStyle: {
+        flex: 1,
+        textAlign: "center",
+        fontWeight: "500"
+      }
+    };
+  };
+  rendlist() {
+    return (
+      <View style={{ marginBottom: 40 }}>
+        {this.state.list.map((item, index) => (
+          <Item key={index} name={item.name} />
+        ))}
+      </View>
+    );
+  }
   render() {
     return (
-      <View>
-        <ScrollView>
-          {this.state.dataArray.map((item, i) => {
-            return (
-              <Item key={i} id={i} name={item.name} isSwitchOn={item.value} />
-            );
-          })}
-        </ScrollView>
+      <View style={styles.container}>
+        <View style={styles.input}>
+          <View
+            style={{
+              borderBottomColor: "#dbdbdb",
+              borderBottomWidth: 1,
+              height: px2dp(50),
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+            <View style={{ width: 90 }}>
+              <Text style={styles.text}>收件人：</Text>
+            </View>
+            <View style={{ flex: 1 }}>
+              <UselessTextInput
+                multiline={true}
+                numberOfLines={1}
+                onChangeText={text => this.setState({ text })}
+                value={this.state.text}
+              />
+            </View>
+          </View>
+          <View
+            style={{
+              height: px2dp(30),
+              justifyContent: "center"
+            }}
+          >
+            <Text style={styles.text}>用户</Text>
+          </View>
+        </View>
+        <View style={{ flex: 1 }}>
+          <ScrollView>{[this.rendlist()]}</ScrollView>
+        </View>
       </View>
     );
   }
@@ -53,21 +135,25 @@ export default class Write extends Component {
 
 class Item extends Component {
   static propTypes = {
-    name: PropTypes.string.isRequired,
-    isSwitchOn: PropTypes.bool
+    name: PropTypes.string.isRequired
   };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      isSwitchOn: this.props.isSwitchOn
-    };
-  }
-
+  go() {}
   render() {
+    const { name } = this.props;
+    px2dp(40);
     return (
       <View style={styles.item}>
-        <Icon name="ios-menu" size={px2dp(25)} color="#ccc" />
+        <View style={styles.avatar}>
+          <TouchableOpacity onPress={this.go.bind(this, "My")}>
+            <Avatar
+              size={px2dp(30)}
+              image={{
+                uri:
+                  "https://timgsa.baidu.com/timg?image&quality=80&size=b10000_10000&sec=1545814744&di=88720d8768c3beec516c92686d5f2270&src=http://images.freeimages.com/images/large-previews/461/dog-1379928.jpg"
+              }}
+            />
+          </TouchableOpacity>
+        </View>
         <Text
           style={{
             fontSize: theme.actionBar.fontSize,
@@ -75,34 +161,33 @@ class Item extends Component {
             marginLeft: px2dp(20)
           }}
         >
-          {this.props.name}
+          {name}
         </Text>
-        <View
-          style={{ flex: 1, flexDirection: "row", justifyContent: "flex-end" }}
-        >
-          <Switch
-            onValueChange={this._onValueChange.bind(this)}
-            value={this.state.isSwitchOn}
-          />
-        </View>
       </View>
     );
-  }
-
-  _onValueChange(value) {
-    this.setState({
-      isSwitchOn: value
-    });
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgb(244,244,244)"
+  },
+  input: {
+    height: px2dp(80)
+  },
+  text: {
+    paddingLeft: px2dp(20)
+  },
+  avatar: {
+    height: px2dp(40)
+  },
   item: {
     flexDirection: "row",
     height: px2dp(49),
     flex: 1,
     alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: "white",
     paddingLeft: px2dp(20),
     paddingRight: px2dp(20),
     borderBottomColor: "#ccc",
